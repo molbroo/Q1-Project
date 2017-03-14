ff51ee8e47421233de3fe6cd4292736f9360025c
MOLLY BROOKS <MOLLYJBROOKS@GMAIL.COM>
2017-03-14 13:39:30 -0600

    added basic structure, exercises, and exercise info pages

---
+++ chinup.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+  getExerciseInfo()
+  function getExerciseInfo(){
+    var description1 = []
+    var musclesPrimary = []
+    var musclesSecondary = []
+    var musclesPrimeName = []
+    var musclesSecName = []
+    var url = "https://wger.de/api/v2/"
+    $.get(url + "exercise/181/")
+    .then(function(data){
+      description1.push(data.description)
+      musclesPrimary.push(data.muscles)
+      musclesSecondary.push(data.muscles_secondary)
+      $.get(url + "muscle/" + musclesPrimary)
+      .then(function(data){
+        musclesPrimeName.push(data.name)
+      })
+      $.get(url + "muscle/" + musclesSecondary)
+      .then(function(data){
+         musclesSecName.push(data.name)
+      })
+      console.log(musclesSecName)
+      console.log(musclesPrimeName)
+      updatePage(description1, musclesPrimeName, musclesSecName)
+    })
+    function updatePage(description1, musclesPrimeName, musclesSecName){
+      $(".descrip").append(description1)
+      $(".muscles").append("<p>Primary Muscles: " + musclesPrimeName + "</p><p>Secondary Muscles: " + musclesSecName + "</p>")
+    }
+  }
+})
