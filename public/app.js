fa189d5bc6e04b41999f2a06fd6d8a7a5f0bee9c
MOLLY BROOKS <MOLLYJBROOKS@GMAIL.COM>
2017-03-16 20:29:24 -0600

    got rid of button on bench press to go back

--- public/app.js
+++ public/app.js
@@ -21,12 +21,10 @@ $(".days-workout").click(function(){
   days.onchange = populateStorage
   // If navigate away from index.html, value of days is still stored when navigate back to index.html
   // If user hits the back button, then getPlan function takes value from local storage
-  // if (window.location.reload()){
-  //   days = localStorage.getItem("myDays")
-  //   getPlan()
-  // }
-
-
+  $(".brand-logo").click(function(){
+    days = localStorage.getItem("myDays")
+    getPlan()
+  })
 
   $(".Day-1").empty()
   $(".Day-2").empty()
@@ -37,7 +35,6 @@ $(".days-workout").click(function(){
   $(".three").empty()
   $(".four").empty()
 })
-
 function getPlan() {
   var name1 = []
   var name2 = []
